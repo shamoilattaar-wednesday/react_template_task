22491f3a2b52c96987c797401cf05755af007df1
Shamoil <apple@Apples-MacBook-Pro-2.local>
2024-02-22 18:39:12 +0530

    changes from day 3 pr

--- app/containers/Tracks/index.js
+++ app/containers/Tracks/index.js
@@ -207,12 +207,6 @@ export function Track({
   };
   const debouncedHandleOnChange = debounce(handleOnChange, 200);
 
-  Track.propTypes = {
-    tracksData: PropTypes.shape({
-      results: PropTypes.array
-    })
-  };
-
   return (
     <Container>
       <CustomCard maxwidth={maxwidth}>
@@ -254,7 +248,8 @@ Track.propTypes = {
   tracksData: PropTypes.shape({
     totalCount: PropTypes.number,
     incompleteResults: PropTypes.bool,
-    items: PropTypes.array
+    items: PropTypes.array,
+    results: PropTypes.array
   }),
   tracksError: PropTypes.string,
   trackName: PropTypes.string,
